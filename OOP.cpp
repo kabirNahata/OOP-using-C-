50046076b0d11c48c694cc3ef9eade3524ff92f7
Kabir Nahata <kbrnahata@gmail.com>
2025-08-06 18:19:51 +0545

    Encapsulation

--- OOP.cpp
+++ OOP.cpp
@@ -2,35 +2,49 @@
 using std::string;
 
 class Employee{
-public:
+private:
     string Name;
     string Company;
     int Age;
 
-    void introduce_yourself(){
-        std::cout << "Name - " << Name << std::endl;
-        std::cout << "Company - " << Company << std::endl;
-        std::cout << "Age - " << Age << std::endl;
+public:
+    void introduce_yourself() {
+        std::cout << "Name: " << Name << std::endl;
+        std::cout << "Company: " << Company << std::endl;
+        std::cout << "Age: " << Age << std::endl;
     }
-
     Employee(string name,string company,int age){
         Name = name;
         Company = company;
         Age = age;
     }
+
+    void setName(string name){
+        Name = name;
+    }
+    string getName(){
+        return Name;
+    }
+    void setCompany(string company){
+        Company = company;
+    }
+    string getCompany(){
+        return Company;
+    }
+    void setAge(int age){
+        Age = age;
+    }
+    int getAge(){
+        return Age;
+    }
+    
 };
 
 int main()
 {
-    Employee employee1 = Employee("Bikash", "Onepoint", 44);
-    employee1.Name = "Bikash";
-    employee1.Company = "Onepoint";
-    employee1.Age = 44;
+    Employee employee1("Bikash", "Onepoint", 44);
     employee1.introduce_yourself();
 
-    // Employee employee2;
-    // employee2.Name = "Samrajya";
-    // employee2.Company = "Onepoint";
-    // employee2.Age = 24;
-    // employee2.introduce_yourself();
+    employee1.setAge(43);
+    std::cout<<employee1.getName()<<" is "<<employee1.getAge()<< " years old "<< std::endl;
 }
